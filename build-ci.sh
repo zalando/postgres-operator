9007f974ae67a258cfac909e8dae63b43cea9bd1
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2017-01-31 18:32:04 +0100

    Add test build-ci.sh script

---
+++ build-ci.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e -x
+
+team_repo=$GOPATH/src/github.bus.zalan.do/acid
+project_dir=$team_repo/postgres-operator
+
+mkdir -p $team_repo
+
+ln -s $project_dir $PWD
+cd $project_dir
+
+glade install
+make docker push
