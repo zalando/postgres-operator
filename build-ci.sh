6b0c104d0533fb3a05aec82bfd217077f9476901
Oleksandr Shulgin <oleksandr.shulgin@zalando.de>
2017-02-01 12:26:46 +0100

    Add quotes around project workspace and target dirs in build-ci.sh

--- build-ci.sh
+++ build-ci.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e -x
 
-team_repo=$GOPATH/src/github.bus.zalan.do/acid
-project_dir=$team_repo/postgres-operator
+team_repo="$GOPATH/src/github.bus.zalan.do/acid"
+project_dir="$team_repo/postgres-operator"
 
-mkdir -p $team_repo
+mkdir -p "$team_repo"
 
-ln -s $PWD $project_dir
-cd $project_dir
+ln -s "$PWD" "$project_dir"
+cd "$project_dir"
 
 glide install
 make clean docker push
