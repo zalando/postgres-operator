73e7ac613555b59749a7fc50ae8efab0c9138c28
Oleksii Kliukin <porkbird@icloud.com>
2017-05-12 14:57:58 +0200

    Use zalando incubator instead of an old repo

--- build-ci.sh
+++ build-ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e -x
 
-team_repo="$GOPATH/src/github.bus.zalan.do/acid"
+team_repo="$GOPATH/src/github.com/zalando-incubator/"
 project_dir="$team_repo/postgres-operator"
 
 mkdir -p "$team_repo"
