ef003c4f225026485f872a0a0abc903acde25def
Murat Kabilov <murat@kabilov.com>
2017-09-28 11:15:04 +0200

    go fmt

--- pkg/cluster/sync.go
+++ pkg/cluster/sync.go
@@ -228,6 +228,8 @@ func (c *Cluster) syncVolumes() error {
 	if err := c.resizeVolumes(c.Spec.Volume, []volumes.VolumeResizer{&volumes.EBSVolumeResizer{}}); err != nil {
 		return fmt.Errorf("could not sync volumes: %v", err)
 	}
+
 	c.logger.Infof("volumes have been synced successfully")
+
 	return nil
 }
