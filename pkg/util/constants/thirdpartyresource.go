6510c15987d8a0a93d98e222c6e2f9f0fdb7ecd7
Murat Kabilov <murat@kabilov.com>
2017-10-05 18:16:22 +0200

    move from tpr to crd

--- pkg/util/constants/thirdpartyresource.go
+++ pkg/util/constants/thirdpartyresource.go
@@ -2,10 +2,9 @@ package constants
 
 // Different properties of the PostgreSQL Third Party Resources
 const (
-	TPRKind        = "postgresql"
-	TPRGroup       = "acid.zalan.do"
-	TPRDescription = "Managed PostgreSQL clusters"
-	TPRApiVersion  = "v1"
-	TPRName        = TPRKind + "." + TPRGroup
-	ResourceName   = TPRKind + "s"
+	CRDKind       = "postgresql"
+	CRDResource   = "postgresqls"
+	CRDShort      = "pg"
+	CRDGroup      = "acid.zalan.do"
+	CRDApiVersion = "v1"
 )
