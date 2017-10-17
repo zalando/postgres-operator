202f2de988ac451a962bf63603a73f93e20fa485
Murat Kabilov <murat@kabilov.com>
2017-10-17 17:03:50 +0200

    Retry connecting to pg

--- pkg/util/constants/postgresql.go
+++ pkg/util/constants/postgresql.go
@@ -1,8 +1,13 @@
 package constants
 
+import "time"
+
 // PostgreSQL specific constants
 const (
 	DataVolumeName    = "pgdata"
 	PostgresDataMount = "/home/postgres/pgdata"
 	PostgresDataPath  = PostgresDataMount + "/pgroot"
+
+	PostgresConnectRetryTimeout = 2 * time.Minute
+	PostgresConnectTimeout      = 15 * time.Second
 )
