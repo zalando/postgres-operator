264833cd6a3f1bbfa670a2ffbbbb5f19ec606b31
Sergey Dudoladov <sergey.dudoladov@zalando.de>
2019-04-24 17:44:29 +0200

    push the script

---
+++ e2e/mock_e2e.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# enable unofficial bash strict mode
+set -o errexit
+set -o nounset
+set -o pipefail
+IFS=$'\n\t'
+
+kind create cluster --name kind-m2 --config ./e2e/kind-config-multikind.yaml --loglevel debug
+export KUBECONFIG="$(kind get kubeconfig-path --name="kind-m")"
+kubectl cluster-info
