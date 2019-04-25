86a17e35e455ec6aefc3871817d5cde7a9d1638b
Sergey Dudoladov <sergey.dudoladov@zalando.de>
2019-04-25 14:48:51 +0200

    add tests skeleton

---
+++ e2e/tests.py
@@ -0,0 +1,21 @@
+from kubernetes import client, config
+
+def main():
+
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+
+    body = {
+        "metadata": {
+            "labels": {
+                "lifecycle-status": "ready",
+             }
+        }
+    }
+
+    nodes = ["kind-test-postgres-operator-worker", "kind-test-postgres-operator-worker2", "kind-test-postgres-operator-worker3"]
+    for node in nodes:
+        _ = v1.patch_node(node, body)
+
+if __name__ == '__main__':
+    main()
