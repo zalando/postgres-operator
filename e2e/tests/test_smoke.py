25fea48f8a93630268afe3ddcc499d72327dc874
Sergey Dudoladov <sergey.dudoladov@zalando.de>
2019-05-02 12:04:01 +0200

    fix flake8 warnings

--- e2e/tests/test_smoke.py
+++ e2e/tests/test_smoke.py
@@ -1,20 +1,21 @@
 
-import unittest, yaml, time
-from kubernetes import client, config, utils
-from pprint import pprint
+import unittest
+import time
 import timeout_decorator
 import subprocess
 import git
 
+from kubernetes import client, config, utils
+
+
 class K8sApi:
 
     def __init__(self):
-       self.config = config.load_kube_config()
-       self.k8s_client = client.ApiClient()
-       self.core_v1 = client.CoreV1Api()
-       self.crd_api = client.CustomObjectsApi()
-       self.apps_v1 = client.AppsV1Api()
-
+        self.config = config.load_kube_config()
+        self.k8s_client = client.ApiClient()
+        self.core_v1 = client.CoreV1Api()
+        self.crd = client.CustomObjectsApi()
+        self.apps_v1 = client.AppsV1Api()
 
 
 class SmokeTestCase(unittest.TestCase):
@@ -32,9 +33,10 @@ class SmokeTestCase(unittest.TestCase):
         In the case of test failure the cluster will stay to enable manual examination;
         next invocation of "make e2e" will re-create it.
         '''
-    
-        # HACK 
-        # 1. creating RBAC entites with a separate client fails with "AttributeError: object has no attribute 'select_header_accept'"
+
+        # HACK
+        # 1. creating RBAC entites with a separate client fails
+        #    with "AttributeError: object has no attribute 'select_header_accept'"
         # 2. utils.create_from_yaml cannot create multiple entites from a single file
         subprocess.run(["kubectl", "create", "-f", "manifests/operator-service-account-rbac.yaml"])
 
@@ -43,20 +45,20 @@ class SmokeTestCase(unittest.TestCase):
         for filename in ["configmap.yaml", "postgres-operator.yaml"]:
             path = "manifests/" + filename
             utils.create_from_yaml(k8s_api.k8s_client, path)
-        
+
         # submit most recent operator image built locally; see VERSION in Makefile
         repo = git.Repo(".")
         version = repo.git.describe("--tags", "--always", "--dirty")
-        
+
         body = {
             "spec": {
                 "template": {
                     "spec": {
-                        "containers": [ 
-                        {
-                          "name" : "postgres-operator",
-                          "image": "registry.opensource.zalan.do/acid/postgres-operator:" + version
-                        }
+                        "containers": [
+                            {
+                              "name": "postgres-operator",
+                              "image": "registry.opensource.zalan.do/acid/postgres-operator:" + version
+                            }
                         ]
                     }
                  }
@@ -68,8 +70,8 @@ class SmokeTestCase(unittest.TestCase):
         while pod_phase != 'Running':
             pods = k8s_api.core_v1.list_namespaced_pod('default', label_selector='name=postgres-operator').items
             if pods:
-               operator_pod = pods[0]
-               pod_phase = operator_pod.status.phase
+                operator_pod = pods[0]
+                pod_phase = operator_pod.status.phase
             print("Waiting for the operator pod to start. Current phase of pod lifecycle: " + str(pod_phase))
             time.sleep(5)
 
@@ -80,19 +82,20 @@ class SmokeTestCase(unittest.TestCase):
         while pod_phase != 'Running':
             pods = k8s_api.core_v1.list_namespaced_pod('default', label_selector='spilo-role=master').items
             if pods:
-               operator_pod = pods[0]
-               pod_phase = operator_pod.status.phase
+                operator_pod = pods[0]
+                pod_phase = operator_pod.status.phase
             print("Waiting for the Spilo master pod to start. Current phase: " + str(pod_phase))
             time.sleep(5)
-        
+
     @timeout_decorator.timeout(240)
     def test_master_is_unique(self):
         """
            Check that there is a single pod in the k8s cluster with the label "spilo-role=master".
         """
         k8s = K8sApi()
-        master_pods = k8s.core_v1.list_namespaced_pod('default', label_selector='spilo-role=master,version=acid-minimal-cluster').items
-        self.assertEqual(len(master_pods), 1, "Expected 1 master pod,found " + str(len(master_pods)))
+        labels = 'spilo-role=master,version=acid-minimal-cluster'
+        master_pods = k8s.core_v1.list_namespaced_pod('default', label_selector=labels).items
+        self.assertEqual(len(master_pods), 1, "Expected 1 master pod, found " + str(len(master_pods)))
 
     @timeout_decorator.timeout(240)
     def test_scaling(self):
@@ -106,12 +109,14 @@ class SmokeTestCase(unittest.TestCase):
                 "numberOfInstances": 3
             }
         }
-        _ = k8s.crd_api.patch_namespaced_custom_object("acid.zalan.do", "v1", "default", "postgresqls", "acid-minimal-cluster", body)
+        _ = k8s.crd.patch_namespaced_custom_object("acid.zalan.do",
+                                                   "v1", "default", "postgresqls", "acid-minimal-cluster", body)
 
-        while len(k8s.core_v1.list_namespaced_pod('default', label_selector='version=acid-minimal-cluster').items) != 3:
+        labels = 'version=acid-minimal-cluster'
+        while len(k8s.core_v1.list_namespaced_pod('default', label_selector=labels).items) != 3:
             print("Waiting for the cluster to scale up to 3 pods.")
             time.sleep(5)
-        self.assertEqual(3, len(k8s.core_v1.list_namespaced_pod('default', label_selector='version=acid-minimal-cluster').items))
+        self.assertEqual(3, len(k8s.core_v1.list_namespaced_pod('default', label_selector=labels).items))
 
         body = {
             "spec": {
@@ -119,12 +124,14 @@ class SmokeTestCase(unittest.TestCase):
             }
         }
 
-        _ = k8s.crd_api.patch_namespaced_custom_object("acid.zalan.do", "v1", "default", "postgresqls", "acid-minimal-cluster", body)
+        _ = k8s.crd.patch_namespaced_custom_object("acid.zalan.do",
+                                                   "v1", "default", "postgresqls", "acid-minimal-cluster", body)
 
-        while len(k8s.core_v1.list_namespaced_pod('default', label_selector='version=acid-minimal-cluster').items) != 2:
+        while len(k8s.core_v1.list_namespaced_pod('default', label_selector=labels).items) != 2:
             print("Waiting for the cluster to scale down to 2 pods.")
             time.sleep(5)
-        self.assertEqual(2, len(k8s.core_v1.list_namespaced_pod('default', label_selector='version=acid-minimal-cluster').items))
+        self.assertEqual(2, len(k8s.core_v1.list_namespaced_pod('default', label_selector=labels).items))
+
 
 if __name__ == '__main__':
     unittest.main()
