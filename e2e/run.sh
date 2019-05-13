0ec5a2c8b629117bfc4a83cab7022db6766e7653
Sergey Dudoladov <sergey.dudoladov@zalando.de>
2019-05-13 13:08:42 +0200

    use full binary name

--- e2e/run.sh
+++ e2e/run.sh
@@ -12,17 +12,16 @@ readonly e2e_test_image=${cluster_name}
 readonly kind_api_server_port=6443 # well-known in the 'kind' codebase
 readonly kubeconfig_path="./e2e/kind-config-${cluster_name}"
 
-alias kind="${GOPATH}/bin/kind-linux-amd64"
 # avoid interference with previous test runs
-if [[ $(kind get clusters | grep "^${cluster_name}*") != "" ]]
+if [[ $(kind-linux-amd64 get clusters | grep "^${cluster_name}*") != "" ]]
 then
-  kind delete cluster --name ${cluster_name}
+  kind-linux-amd64 delete cluster --name ${cluster_name}
 fi
 
-kind create cluster --name ${cluster_name} --config ./e2e/kind-cluster-postgres-operator-e2e-tests.yaml
-export KUBECONFIG="$(kind get kubeconfig-path --name=${cluster_name})"
+kind-linux-amd64 create cluster --name ${cluster_name} --config ./e2e/kind-cluster-postgres-operator-e2e-tests.yaml
+export KUBECONFIG="$(kind-linux-amd64 get kubeconfig-path --name=${cluster_name})"
 
-kind load docker-image ${operator_image} --name ${cluster_name}
+kind-linux-amd64 load docker-image ${operator_image} --name ${cluster_name}
 
 # use the actual kubeconfig to connect to the 'kind' API server
 # but update the IP address of the API server to the one from the Docker 'bridge' network
@@ -32,6 +31,5 @@ sed -i "s/server.*$/server: https:\/\/$kind_api_server/g" ${kubeconfig_path}
 
 docker run --rm --mount type=bind,source="$(realpath ${kubeconfig_path})",target=/root/.kube/config -e OPERATOR_IMAGE=${operator_image} ${e2e_test_image}
 
-kind delete cluster --name ${cluster_name}
-unalias kind
+kind-linux-amd64 delete cluster --name ${cluster_name}
 rm -rf ${kubeconfig_path}
