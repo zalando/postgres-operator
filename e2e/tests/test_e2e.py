04a67641abfc0ec3b2127a0a48fcb57f261efa9b
Felix Kunde <felix-kunde@gmx.de>
2020-04-20 15:05:05 +0200

    subsequent updates of config

--- e2e/tests/test_e2e.py
+++ e2e/tests/test_e2e.py
@@ -215,10 +215,17 @@ class EndToEndTestCase(unittest.TestCase):
         cluster_label = 'application=spilo,cluster-name=acid-minimal-cluster'
 
         # enable lazy update
+        patch_lazy_spilo_upgrade = {
+            "data": {
+                "enable_lazy_spilo_upgrade": "true"
+            }
+        }
+        k8s.update_config(patch_lazy_spilo_upgrade)
+
+        # update docker image in config which usually triggers a rolling update
         conf_image = "registry.opensource.zalan.do/acid/spilo-cdp-12:1.6-p111"
         patch_lazy_spilo_upgrade = {
             "data": {
-                "enable_lazy_spilo_upgrade": "true",
                 "docker_image": conf_image
             }
         }
@@ -236,7 +243,7 @@ class EndToEndTestCase(unittest.TestCase):
 
         # sanity check: restarted pod runs the image specified in operator's conf
         new_image = k8s.get_effective_pod_image(replica_pod)
-        self.assertEqual(conf_image, new_image,
+        self.assertEqual(new_image, conf_image
             "Lazy upgrade failed: restarted pod {} runs image {} different from the one in operator conf {}".format(replica_pod, new_image, conf_image))
 
         # lazy update works if the restarted pod and older pods have different Spilo versions
