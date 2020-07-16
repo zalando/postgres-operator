a315c90e7156c19bf75bc50f7787c4b250eafa13
Felix Kunde <felix.kunde@zalando.de>
2020-07-16 17:15:06 +0200

    set search_path for default roles

--- pkg/util/constants/roles.go
+++ pkg/util/constants/roles.go
@@ -2,20 +2,21 @@ package constants
 
 // Roles specific constants
 const (
-	PasswordLength            = 64
-	SuperuserKeyName          = "superuser"
+	PasswordLength              = 64
+	SuperuserKeyName            = "superuser"
 	ConnectionPoolerUserKeyName = "pooler"
-	ReplicationUserKeyName    = "replication"
-	RoleFlagSuperuser         = "SUPERUSER"
-	RoleFlagInherit           = "INHERIT"
-	RoleFlagLogin             = "LOGIN"
-	RoleFlagNoLogin           = "NOLOGIN"
-	RoleFlagCreateRole        = "CREATEROLE"
-	RoleFlagCreateDB          = "CREATEDB"
-	RoleFlagReplication       = "REPLICATION"
-	RoleFlagByPassRLS         = "BYPASSRLS"
-	OwnerRoleNameSuffix       = "_owner"
-	ReaderRoleNameSuffix      = "_reader"
-	WriterRoleNameSuffix      = "_writer"
-	UserRoleNameSuffix        = "_user"
+	ReplicationUserKeyName      = "replication"
+	RoleFlagSuperuser           = "SUPERUSER"
+	RoleFlagInherit             = "INHERIT"
+	RoleFlagLogin               = "LOGIN"
+	RoleFlagNoLogin             = "NOLOGIN"
+	RoleFlagCreateRole          = "CREATEROLE"
+	RoleFlagCreateDB            = "CREATEDB"
+	RoleFlagReplication         = "REPLICATION"
+	RoleFlagByPassRLS           = "BYPASSRLS"
+	OwnerRoleNameSuffix         = "_owner"
+	ReaderRoleNameSuffix        = "_reader"
+	WriterRoleNameSuffix        = "_writer"
+	UserRoleNameSuffix          = "_user"
+	DefaultSearchPath           = "\"$user\""
 )
