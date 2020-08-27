70da322f965552d13987aca27c5dd2d00942bf66
Felix Kunde <felix-kunde@gmx.de>
2020-08-27 18:27:16 +0200

    revert codegen

--- pkg/generated/clientset/versioned/fake/register.go
+++ pkg/generated/clientset/versioned/fake/register.go
@@ -35,7 +35,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
-
+var parameterCodec = runtime.NewParameterCodec(scheme)
 var localSchemeBuilder = runtime.SchemeBuilder{
 	acidv1.AddToScheme,
 }
