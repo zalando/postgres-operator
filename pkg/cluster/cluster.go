fe12184f1790f33926bf5c6097ac4a8153cb3882
Felix Kunde <felix-kunde@gmx.de>
2020-11-06 17:22:52 +0100

    just one debug line for syncing volumes

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -672,15 +672,13 @@ func (c *Cluster) Update(oldSpec, newSpec *acidv1.Postgresql) error {
 	// Volume
 	if oldSpec.Spec.Size != newSpec.Spec.Size {
 		c.logVolumeChanges(oldSpec.Spec.Volume, newSpec.Spec.Volume)
-
+		c.logger.Debugf("syncing volumes using %q resize mode", c.OpConfig.StorageResizeMode)
 		if c.OpConfig.StorageResizeMode == "pvc" {
-			c.logger.Debugf("syncing persistent volume claims using %q resize mode", c.OpConfig.StorageResizeMode)
 			if err := c.syncVolumeClaims(); err != nil {
 				c.logger.Errorf("could not sync persistent volume claims: %v", err)
 				updateFailed = true
 			}
 		} else if c.OpConfig.StorageResizeMode == "ebs" {
-			c.logger.Debugf("syncing persistent volumes using %q resize mode", c.OpConfig.StorageResizeMode)
 			if err := c.syncVolumes(); err != nil {
 				c.logger.Errorf("could not sync persistent volumes: %v", err)
 				updateFailed = true
