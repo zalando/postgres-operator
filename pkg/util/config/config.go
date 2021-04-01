c18241f187d22ddddf330588a425ec799c131725
Felix Kunde <felix-kunde@gmx.de>
2021-04-01 11:53:07 +0200

    Bump v1.6.2 (#1433)
    * helm chart remove 1.6.0 archive from 1.6.0 archive
    
    * bump operator to v1.6.2
    
    * fix pointer deref
    
    * skip connection pooler sync when empty
    
    * revert pooler change and minor update to version msg
    
    * do not log query on error when creating or altering users

--- pkg/util/config/config.go
+++ pkg/util/config/config.go
@@ -23,40 +23,40 @@ type CRD struct {
 
 // Resources describes kubernetes resource specific configuration parameters
 type Resources struct {
-	ResourceCheckInterval     time.Duration       `name:"resource_check_interval" default:"3s"`
-	ResourceCheckTimeout      time.Duration       `name:"resource_check_timeout" default:"10m"`
-	PodLabelWaitTimeout       time.Duration       `name:"pod_label_wait_timeout" default:"10m"`
-	PodDeletionWaitTimeout    time.Duration       `name:"pod_deletion_wait_timeout" default:"10m"`
-	PodTerminateGracePeriod   time.Duration       `name:"pod_terminate_grace_period" default:"5m"`
-	SpiloRunAsUser            *int64              `name:"spilo_runasuser,omitempty"`
-	SpiloRunAsGroup           *int64              `name:"spilo_runasgroup,omitempty"`
-	SpiloFSGroup              *int64              `name:"spilo_fsgroup"`
-	PodPriorityClassName      string              `name:"pod_priority_class_name"`
-	ClusterDomain             string              `name:"cluster_domain" default:"cluster.local"`
-	SpiloPrivileged           bool                `name:"spilo_privileged" default:"false"`
-	SpiloAllowPrivilegeEscalation bool            `name:"spilo_allow_privilege_escalation" default:"false"`
-	AdditionalPodCapabilities []string            `name:"additional_pod_capabilities" default:""`
-	ClusterLabels             map[string]string   `name:"cluster_labels" default:"application:spilo"`
-	InheritedLabels           []string            `name:"inherited_labels" default:""`
-	InheritedAnnotations      []string            `name:"inherited_annotations" default:""`
-	DownscalerAnnotations     []string            `name:"downscaler_annotations"`
-	ClusterNameLabel          string              `name:"cluster_name_label" default:"cluster-name"`
-	DeleteAnnotationDateKey   string              `name:"delete_annotation_date_key"`
-	DeleteAnnotationNameKey   string              `name:"delete_annotation_name_key"`
-	PodRoleLabel              string              `name:"pod_role_label" default:"spilo-role"`
-	PodToleration             map[string]string   `name:"toleration" default:""`
-	DefaultCPURequest         string              `name:"default_cpu_request" default:"100m"`
-	DefaultMemoryRequest      string              `name:"default_memory_request" default:"100Mi"`
-	DefaultCPULimit           string              `name:"default_cpu_limit" default:"1"`
-	DefaultMemoryLimit        string              `name:"default_memory_limit" default:"500Mi"`
-	MinCPULimit               string              `name:"min_cpu_limit" default:"250m"`
-	MinMemoryLimit            string              `name:"min_memory_limit" default:"250Mi"`
-	PodEnvironmentConfigMap   spec.NamespacedName `name:"pod_environment_configmap"`
-	PodEnvironmentSecret      string              `name:"pod_environment_secret"`
-	NodeReadinessLabel        map[string]string   `name:"node_readiness_label" default:""`
-	MaxInstances              int32               `name:"max_instances" default:"-1"`
-	MinInstances              int32               `name:"min_instances" default:"-1"`
-	ShmVolume                 *bool               `name:"enable_shm_volume" default:"true"`
+	ResourceCheckInterval         time.Duration       `name:"resource_check_interval" default:"3s"`
+	ResourceCheckTimeout          time.Duration       `name:"resource_check_timeout" default:"10m"`
+	PodLabelWaitTimeout           time.Duration       `name:"pod_label_wait_timeout" default:"10m"`
+	PodDeletionWaitTimeout        time.Duration       `name:"pod_deletion_wait_timeout" default:"10m"`
+	PodTerminateGracePeriod       time.Duration       `name:"pod_terminate_grace_period" default:"5m"`
+	SpiloRunAsUser                *int64              `name:"spilo_runasuser,omitempty"`
+	SpiloRunAsGroup               *int64              `name:"spilo_runasgroup,omitempty"`
+	SpiloFSGroup                  *int64              `name:"spilo_fsgroup"`
+	PodPriorityClassName          string              `name:"pod_priority_class_name"`
+	ClusterDomain                 string              `name:"cluster_domain" default:"cluster.local"`
+	SpiloPrivileged               bool                `name:"spilo_privileged" default:"false"`
+	SpiloAllowPrivilegeEscalation *bool               `name:"spilo_allow_privilege_escalation" default:"true"`
+	AdditionalPodCapabilities     []string            `name:"additional_pod_capabilities" default:""`
+	ClusterLabels                 map[string]string   `name:"cluster_labels" default:"application:spilo"`
+	InheritedLabels               []string            `name:"inherited_labels" default:""`
+	InheritedAnnotations          []string            `name:"inherited_annotations" default:""`
+	DownscalerAnnotations         []string            `name:"downscaler_annotations"`
+	ClusterNameLabel              string              `name:"cluster_name_label" default:"cluster-name"`
+	DeleteAnnotationDateKey       string              `name:"delete_annotation_date_key"`
+	DeleteAnnotationNameKey       string              `name:"delete_annotation_name_key"`
+	PodRoleLabel                  string              `name:"pod_role_label" default:"spilo-role"`
+	PodToleration                 map[string]string   `name:"toleration" default:""`
+	DefaultCPURequest             string              `name:"default_cpu_request" default:"100m"`
+	DefaultMemoryRequest          string              `name:"default_memory_request" default:"100Mi"`
+	DefaultCPULimit               string              `name:"default_cpu_limit" default:"1"`
+	DefaultMemoryLimit            string              `name:"default_memory_limit" default:"500Mi"`
+	MinCPULimit                   string              `name:"min_cpu_limit" default:"250m"`
+	MinMemoryLimit                string              `name:"min_memory_limit" default:"250Mi"`
+	PodEnvironmentConfigMap       spec.NamespacedName `name:"pod_environment_configmap"`
+	PodEnvironmentSecret          string              `name:"pod_environment_secret"`
+	NodeReadinessLabel            map[string]string   `name:"node_readiness_label" default:""`
+	MaxInstances                  int32               `name:"max_instances" default:"-1"`
+	MinInstances                  int32               `name:"min_instances" default:"-1"`
+	ShmVolume                     *bool               `name:"enable_shm_volume" default:"true"`
 }
 
 type InfrastructureRole struct {
@@ -114,7 +114,7 @@ type Scalyr struct {
 // LogicalBackup defines configuration for logical backup
 type LogicalBackup struct {
 	LogicalBackupSchedule                     string `name:"logical_backup_schedule" default:"30 00 * * *"`
-	LogicalBackupDockerImage                  string `name:"logical_backup_docker_image" default:"registry.opensource.zalan.do/acid/logical-backup:v1.6.1"`
+	LogicalBackupDockerImage                  string `name:"logical_backup_docker_image" default:"registry.opensource.zalan.do/acid/logical-backup:v1.6.2"`
 	LogicalBackupProvider                     string `name:"logical_backup_provider" default:"s3"`
 	LogicalBackupS3Bucket                     string `name:"logical_backup_s3_bucket" default:""`
 	LogicalBackupS3Region                     string `name:"logical_backup_s3_region" default:""`
@@ -152,7 +152,7 @@ type Config struct {
 	WatchedNamespace        string            `name:"watched_namespace"` // special values: "*" means 'watch all namespaces', the empty string "" means 'watch a namespace where operator is deployed to'
 	KubernetesUseConfigMaps bool              `name:"kubernetes_use_configmaps" default:"false"`
 	EtcdHost                string            `name:"etcd_host" default:""` // special values: the empty string "" means Patroni will use K8s as a DCS
-	DockerImage             string            `name:"docker_image" default:"registry.opensource.zalan.do/acid/spilo-13:2.0-p4"`
+	DockerImage             string            `name:"docker_image" default:"registry.opensource.zalan.do/acid/spilo-13:2.0-p6"`
 	SidecarImages           map[string]string `name:"sidecar_docker_images"` // deprecated in favour of SidecarContainers
 	SidecarContainers       []v1.Container    `name:"sidecars"`
 	PodServiceAccountName   string            `name:"pod_service_account_name" default:"postgres-pod"`
