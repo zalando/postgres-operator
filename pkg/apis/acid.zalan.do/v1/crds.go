37a5e8b967a2e0a95d3daddbcf1148febc2c160c
Stef Graces <sgraces@sofico.be>
2021-11-02 16:39:41 +0100

    Add azure config

--- pkg/apis/acid.zalan.do/v1/crds.go
+++ pkg/apis/acid.zalan.do/v1/crds.go
@@ -1403,6 +1403,18 @@ var OperatorConfigCRDResourceValidation = apiextv1.CustomResourceValidation{
 								Type:    "string",
 								Pattern: "^(\\d+|\\*)(/\\d+)?(\\s+(\\d+|\\*)(/\\d+)?){4}$",
 							},
+							"logical_backup_storage_account_name": {
+								Type: "string",
+							},
+							"logical_backup_storage_container": {
+								Type: "string",
+							},
+							"logical_backup_storage_blob": {
+								Type: "string",
+							},
+							"logical_backup_storage_account_key": {
+								Type: "string",
+							},
 						},
 					},
 					"debug": {
