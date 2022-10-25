8a853e151d8ea27fe3a5c27ed1c670d41c6733b7
Stephan Austermühle <au@hcsd.de>
2022-10-25 11:55:20 +0200

    Add support for fsGroupChangePolicy
    Signed-off-by: Stephan Austermühle <au@hcsd.de>

--- pkg/apis/acid.zalan.do/v1/operator_configuration_type.go
+++ pkg/apis/acid.zalan.do/v1/operator_configuration_type.go
@@ -60,6 +60,7 @@ type KubernetesMetaConfiguration struct {
 	PodServiceAccountDefinition            string                       `json:"pod_service_account_definition,omitempty"`
 	PodServiceAccountRoleBindingDefinition string                       `json:"pod_service_account_role_binding_definition,omitempty"`
 	PodTerminateGracePeriod                Duration                     `json:"pod_terminate_grace_period,omitempty"`
+	PodFsGroupChangePolicy                 v1.PodFSGroupChangePolicy    `json:"pod_fsgroupchangepolicy,omitempty"`
 	SpiloPrivileged                        bool                         `json:"spilo_privileged,omitempty"`
 	SpiloAllowPrivilegeEscalation          *bool                        `json:"spilo_allow_privilege_escalation,omitempty"`
 	SpiloRunAsUser                         *int64                       `json:"spilo_runasuser,omitempty"`
