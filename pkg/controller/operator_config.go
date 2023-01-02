be7b52db92d774c196ddb62a9de6fcbcfac7228e
Francois Parquet <francois.parquet@gmail.com>
2023-01-02 18:22:47 +0100

    add preferred during scheduling pod anti affinity (#2048)
    * add preferred during scheduling pod anti affinity
    
    Co-authored-by: Felix Kunde <felix-kunde@gmx.de>

--- pkg/controller/operator_config.go
+++ pkg/controller/operator_config.go
@@ -123,6 +123,7 @@ func (c *Controller) importConfigurationFromCRD(fromCRD *acidv1.OperatorConfigur
 	result.MasterPodMoveTimeout = util.CoalesceDuration(time.Duration(fromCRD.Kubernetes.MasterPodMoveTimeout), "10m")
 	result.EnablePodAntiAffinity = fromCRD.Kubernetes.EnablePodAntiAffinity
 	result.PodAntiAffinityTopologyKey = util.Coalesce(fromCRD.Kubernetes.PodAntiAffinityTopologyKey, "kubernetes.io/hostname")
+	result.PodAntiAffinityPreferredDuringScheduling = fromCRD.Kubernetes.PodAntiAffinityPreferredDuringScheduling
 	result.PodToleration = fromCRD.Kubernetes.PodToleration
 
 	// Postgres Pod resources
