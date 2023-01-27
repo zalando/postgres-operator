49575634e422ce681df862b2b869e1b266264e99
mkrueger92 <7305571+mkrueger92@users.noreply.github.com>
2023-01-27 16:51:35 +0100

    Move CLUSTER_ID to end of list
    This (hopefully) prevents tests from failing.

--- pkg/cluster/k8sres.go
+++ pkg/cluster/k8sres.go
@@ -993,9 +993,6 @@ func (c *Cluster) generateSpiloPodEnvVars(
 		envVars = append(envVars, c.generateStandbyEnvironment(spec.StandbyCluster)...)
 	}
 
-    // add variable containing the UID of the cluster; usefull for backup paths
-	envVars = append(envVars, v1.EnvVar{Name: "CLUSTER_UID", Value: getBucketScopeSuffix(string(uid))})
-
 	// fetch variables from custom environment Secret
 	// that will override all subsequent global variables
 	secretEnvVarsList, err := c.getPodEnvironmentSecretVariables()
@@ -1044,6 +1041,9 @@ func (c *Cluster) generateSpiloPodEnvVars(
 
 	envVars = appendEnvVars(envVars, opConfigEnvVars...)
 
+	// add variable containing the UID of the cluster; usefull for backup paths
+	envVars = append(envVars, v1.EnvVar{Name: "CLUSTER_UID", Value: getBucketScopeSuffix(string(uid))})
+	
 	return envVars, nil
 }
 
