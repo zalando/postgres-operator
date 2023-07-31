4833af2371a2f1ff60a2569a8c9cf3a459017857
inovindasari <ida.novindasari@zalando.de>
2023-07-31 16:22:54 +0200

    skip node affinity test

--- e2e/tests/test_e2e.py
+++ e2e/tests/test_e2e.py
@@ -1358,6 +1358,7 @@ class EndToEndTestCase(unittest.TestCase):
             time.sleep(5)
 
     @timeout_decorator.timeout(TEST_TIMEOUT_SEC)
+    @unittest.skip("Skipping this test until fixed")
     def test_node_affinity(self):
         '''
            Add label to a node and update postgres cluster spec to deploy only on a node with that label
