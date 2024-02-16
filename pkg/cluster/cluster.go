4e011669a3f30ca35d8c39d37aa5be1aae9708a3
Felix Kunde <felix-kunde@gmx.de>
2024-02-16 11:55:09 +0100

    bump go version and update dependencies

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -1163,7 +1163,7 @@ func (c *Cluster) ReceivePodEvent(event PodEvent) {
 	}
 }
 
-func (c *Cluster) processPodEvent(obj interface{}) error {
+func (c *Cluster) processPodEvent(obj interface{}, isInInitialList bool) error {
 	event, ok := obj.(PodEvent)
 	if !ok {
 		return fmt.Errorf("could not cast to PodEvent")
