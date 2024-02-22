5b9d8f90a1357853e6db9bc4c2a45fabe4315453
Felix Kunde <felix-kunde@gmx.de>
2024-02-22 13:44:40 +0100

    switch to kube_codegen in update-codegen

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -8,18 +8,33 @@ GENERATED_PACKAGE_ROOT="github.com"
 OPERATOR_PACKAGE_ROOT="${GENERATED_PACKAGE_ROOT}/zalando/postgres-operator"
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
 TARGET_CODE_DIR=${1-${SCRIPT_ROOT}/pkg}
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo "${GOPATH}"/src/k8s.io/code-generator)}
+CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
+
+source "${CODEGEN_PKG}/kube_codegen.sh"
 
 cleanup() {
     rm -rf "${GENERATED_PACKAGE_ROOT}"
 }
 trap "cleanup" EXIT SIGINT
 
-bash "${CODEGEN_PKG}/kube_codegen.sh" client,deepcopy,informer,lister \
-  "${OPERATOR_PACKAGE_ROOT}/pkg/generated" "${OPERATOR_PACKAGE_ROOT}/pkg/apis" \
-  "acid.zalan.do:v1 zalando.org:v1" \
-  --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt \
-  -o ./
+kube::codegen::gen_helpers \
+    --input-pkg-root "${OPERATOR_PACKAGE_ROOT}/pkg/apis" \
+    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../../.." \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+
+kube::codegen::gen_client \
+    --with-watch \
+    --with-applyconfig \
+    --input-pkg-root "${OPERATOR_PACKAGE_ROOT}/pkg/apis" \
+    --output-pkg-root "${OPERATOR_PACKAGE_ROOT}/pkg/generated/client" \
+    --output-base "$(dirname "${BASH_SOURCE[0]}")/../../../../.." \
+    --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+
+#bash "${CODEGEN_PKG}/kube_codegen.sh" client,deepcopy,informer,lister \
+#  "${OPERATOR_PACKAGE_ROOT}/pkg/generated" "${OPERATOR_PACKAGE_ROOT}/pkg/apis" \
+#  "acid.zalan.do:v1 zalando.org:v1" \
+#  --go-header-file "${SCRIPT_ROOT}"/hack/custom-boilerplate.go.txt \
+#  -o ./
 
 cp -r "${OPERATOR_PACKAGE_ROOT}"/pkg/* "${TARGET_CODE_DIR}"
 
