85b8058029fd5289373d77c32f444a723928869b
Felix Kunde <felix-kunde@gmx.de>
2024-08-09 14:47:23 +0200

    bump spilo to 16-3.3, drop support for pg11 (#2706)
    * bump spilo to 16-3.3, drop support for pg11
    * update README

--- pkg/apis/acid.zalan.do/v1/crds.go
+++ pkg/apis/acid.zalan.do/v1/crds.go
@@ -596,9 +596,6 @@ var PostgresCRDResourceValidation = apiextv1.CustomResourceValidation{
 								Type: "string",
 								Enum: []apiextv1.JSON{
 									{
-										Raw: []byte(`"11"`),
-									},
-									{
 										Raw: []byte(`"12"`),
 									},
 									{
