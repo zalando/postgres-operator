6fdf2c92fa71e1bf3d99deff35401fd93d736086
Yingrjimsch <nobel98@bluewin.ch>
2024-09-05 14:45:23 +0200

    fix(2582): added cluster level delete secrets config
    chore: removed log messages
    
    chore: removed test yaml
    
    Revert "chore: removed test yaml"
    
    This reverts commit f19110c86406579133f79637c3a053053857e072.
    
    chore: removed test yaml
    
    chore: added docs
    
    chore: remove accident file

--- pkg/apis/acid.zalan.do/v1/crds.go
+++ pkg/apis/acid.zalan.do/v1/crds.go
@@ -1344,6 +1344,9 @@ var OperatorConfigCRDResourceValidation = apiextv1.CustomResourceValidation{
 							"enable_secrets_deletion": {
 								Type: "boolean",
 							},
+							"enable_secrets_deletion_key": {
+								Type: "string",
+							},
 							"enable_sidecars": {
 								Type: "boolean",
 							},
