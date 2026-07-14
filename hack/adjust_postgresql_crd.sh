69936feb1f2fb7666d827523836d204e178c5c3c
tcondeixa <tscondeixa@gmail.com>
2026-07-14 13:54:25 +0200

    revert the changes on sed and date

--- hack/adjust_postgresql_crd.sh
+++ hack/adjust_postgresql_crd.sh
@@ -13,14 +13,12 @@
 
 file="${1:-"manifests/postgresql.crd.yaml"}"
 
-SED=$(command -v gsed 2>/dev/null || command -v sed)
-
-$SED -i '/^[[:space:]]*standby:$/{
+sed -i '/^[[:space:]]*standby:$/{
     # Capture the indentation
     s/^\([[:space:]]*\)standby:$/\1standby:\n\1  anyOf:\n\1  - required:\n\1    - s3_wal_path\n\1  - required:\n\1    - gs_wal_path\n\1  - required:\n\1    - standby_host\n\1  not:\n\1    required:\n\1    - s3_wal_path\n\1    - gs_wal_path/
 }' "$file"
 
-$SED -i '/^[[:space:]]*maintenanceWindows:$/{
+sed -i '/^[[:space:]]*maintenanceWindows:$/{
     # Capture the indentation
     s/^\([[:space:]]*\)maintenanceWindows:$/\1maintenanceWindows:\n\1  items:\n\1    pattern: '\''^\\ *((Mon|Tue|Wed|Thu|Fri|Sat|Sun):(2[0-3]|[01]?\\d):([0-5]?\\d)|(2[0-3]|[01]?\\d):([0-5]?\\d))-((2[0-3]|[01]?\\d):([0-5]?\\d)|(2[0-3]|[01]?\\d):([0-5]?\\d))\\ *$'\''\n\1    type: string/
 }' "$file"
